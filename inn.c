eabb6b02dacd66970e660385aa55ae66ce4e1ba8
Tcharlyss <charlysjerfeson@gmail.com>
2023-05-03 16:22:27 -0300

    add branch

--- inn.c
+++ inn.c
@@ -1,7 +1,7 @@
 //Implementação das funções do módulo aluno.h.
 
-#include<stdio.h>  //Biblioteca de entrada e saída.
-#include<string.h> //Biblioteca de funções para manipular strings.
-#include<stdlib.h> //Biblioteca de funções para alocação dinâmica.
-#include"pousada.h"  //Módulo criado.
+#include <stdio.h>  //Biblioteca de entrada e saída.
+#include <string.h> //Biblioteca de funções para manipular strings.
+#include <stdlib.h> //Biblioteca de funções para alocação dinâmica.
+#include "pousada.h"  //Módulo criado.
 
