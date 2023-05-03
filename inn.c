980702c8d5ffa96e67e65619ec14984c203cd6ea
gabriel <uyanelol2016@gmail.com>
2023-05-03 16:23:51 -0300

    test

--- inn.c
+++ inn.c
@@ -1,7 +1,6 @@
-//Implementação das funções do módulo aluno.h.
 
-#include<stdio.h>  //Biblioteca de entrada e saída.
-#include<string.h> //Biblioteca de funções para manipular strings.
-#include<stdlib.h> //Biblioteca de funções para alocação dinâmica.
-#include"pousada.h"  //Módulo criado.
+#include<stdio.h>  
+#include<string.h> 
+#include<stdlib.h> 
+#include"inn.h"  
 
